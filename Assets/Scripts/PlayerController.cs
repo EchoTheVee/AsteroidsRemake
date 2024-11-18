1e6f62a61c199e07d143c965a25e8a5226d4ebf6
EchoTheVee <taikorodon@gmail.com>
2024-11-18 09:51:28 -0800

    Iteration 1 to 3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float verticalInput;
+    private float horizontalInput;
+    public float turnSpeed;
+    public float moveSpeed;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+        playerRb.AddRelativeForce(Vector3.forward * moveSpeed * verticalInput);
+    }
+}
