a9bcb2c24bd8eda2c4cf3f15309f611a147e851c
EchoTheVee <taikorodon@gmail.com>
2024-11-26 09:49:26 -0800

    Added Ship Customization

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,13 +16,18 @@ public class GameManager : MonoBehaviour
     public AudioClip onHit;
     private int highScore;
     public Camera main;
+    public Slider colorSlider;
+    public MeshRenderer mr;
+    public Material[] colors;
+    public int colorValue;
 
     // Start is called before the first frame update
     void Start()
     {
+        colorSlider.value = PlayerPrefs.GetInt("colorValue");
+        mr = GameObject.Find("PlayerMR").GetComponent<MeshRenderer>();
         highScore = PlayerPrefs.GetInt("highScore");
         highScoreDisplay.text = $"HighScore: {highScore}";
-
         score = 0;
         UpdateScore();
     }
@@ -29,7 +35,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //highScore = PlayerPrefs.GetInt("highScore");
+        highScore = PlayerPrefs.GetInt("highScore");
 
         healthDisplay.text = $"Health: {hp}";
 
@@ -37,6 +43,19 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene("GameOverScreen");
         }
+        mr.material = colors[Mathf.RoundToInt(colorSlider.value)];
+        /*
+        if (colorSlider.value == 1)
+        {
+            mr.material = purple;
+        }
+
+        if (colorSlider.value == 2)
+        {
+            mr.material = yellow;
+        }
+*/
+
     }
 
     public void UpdateScore()
@@ -55,5 +74,12 @@ public class GameManager : MonoBehaviour
     {
         score += amountToAdd;
         UpdateScore();
+    }
+
+    public void ColorValueUpdate()
+    {
+        colorValue = (int) colorSlider.value;
+        
+        PlayerPrefs.SetInt("colorValue", colorValue);
     }
 }
